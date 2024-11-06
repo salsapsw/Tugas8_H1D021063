c0ede3a416603df33a9675b7e3b830921da250a2
Salsabillapsw <salsabillaputerisw29@gmail.com>
2024-11-06 16:47:59 +0700

    tugas 8

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'mahasiswa',
+    pathMatch: 'full',
+  },
+  {
+    path: 'mahasiswa',
+    loadChildren: () =>
+      import('./mahasiswa/mahasiswa.module').then((m) => m.MahasiswaPageModule),
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
